0b3aeda713b7e61b0716819751851f19bf7af913
julsco <juliancscotti@gmail.com>
2022-12-03 13:12:41 +0000

    fun

--- src/api.js
+++ src/api.js
@@ -1,96 +1,22 @@
-const express = require("express");
-const serverless = require("serverless-http");
+import { teams } from "./teams";
+import { players } from "./players";
 
-const app = express();
-const router = express.Router();
+const buildAPI = (data, path) => {
+  const express = require("express");
+  const serverless = require("serverless-http");
 
-router.get("/", (req, res) => {
-  res.json([
-    {
-      "name": "Arsenal",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t3.svg"
-    },
-    {
-      "name": "Aston Villa",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t7.svg"
-    },
-    {
-      "name": "Bournemouth",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t91.svg"
-    },
-    {
-      "name": "Brentford",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t94.svg"
-    },
-    {
-      "name": "Brighton and Hove Albion",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t36.svg"
-    },
-    {
-      "name": "Chelsea",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t8.svg"
-    },
-    {
-      "name": "Crystal Palace",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t31.svg"
-    },
-    {
-      "name": "Everton",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t11.svg"
-    },
-    {
-      "name": "Fulham",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t54.svg"
-    },
-    {
-      "name": "Leeds United",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t2.svg"
-    },
-    {
-      "name": "Leicester City",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t13.svg"
-    },
-    {
-      "name": "Liverpool",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t14.svg"
-    },
-    {
-      "name": "Manchester City",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t43.svg"
-    },
-    {
-      "name": "Manchester United",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t1.svg"
-    },
-    {
-      "name": "Newcastle United",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t4.svg"
-    },
-    {
-      "name": "Nottingham Forest",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t17.svg"
-    },
-    {
-      "name": "Southampton",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t20.svg"
-    },
-    {
-      "name": "Tottenham Hotspur",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t6.svg"
-    },
-    {
-      "name": "West Ham United",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t21.svg"
-    },
-    {
-      "name": "Wolverhampton Wanderers",
-      "imgURL": "https://resources.premierleague.com/premierleague/badges/t39.svg"
-    }
-  ]);
-});
+  const app = express();
+  const router = express.Router();
 
-app.use(`/.netlify/functions/api/teams`, router);
+  router.get("/", (req, res) => {
+    res.json(data);
+  });
 
-module.exports = app;
-module.exports.handler = serverless(app);
+  app.use(`/.netlify/functions/api/${path}`, router);
 
+  module.exports = app;
+  module.exports.handler = serverless(app);
+}
+
+buildAPI(teams, "/teams")
+buildAPI(players, "/players")
---
+++ src/teams.js
@@ -0,0 +1,82 @@
+export const teams = [
+    {
+      "name": "Arsenal",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t3.svg"
+    },
+    {
+      "name": "Aston Villa",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t7.svg"
+    },
+    {
+      "name": "Bournemouth",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t91.svg"
+    },
+    {
+      "name": "Brentford",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t94.svg"
+    },
+    {
+      "name": "Brighton and Hove Albion",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t36.svg"
+    },
+    {
+      "name": "Chelsea",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t8.svg"
+    },
+    {
+      "name": "Crystal Palace",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t31.svg"
+    },
+    {
+      "name": "Everton",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t11.svg"
+    },
+    {
+      "name": "Fulham",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t54.svg"
+    },
+    {
+      "name": "Leeds United",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t2.svg"
+    },
+    {
+      "name": "Leicester City",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t13.svg"
+    },
+    {
+      "name": "Liverpool",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t14.svg"
+    },
+    {
+      "name": "Manchester City",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t43.svg"
+    },
+    {
+      "name": "Manchester United",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t1.svg"
+    },
+    {
+      "name": "Newcastle United",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t4.svg"
+    },
+    {
+      "name": "Nottingham Forest",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t17.svg"
+    },
+    {
+      "name": "Southampton",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t20.svg"
+    },
+    {
+      "name": "Tottenham Hotspur",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t6.svg"
+    },
+    {
+      "name": "West Ham United",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t21.svg"
+    },
+    {
+      "name": "Wolverhampton Wanderers",
+      "imgURL": "https://resources.premierleague.com/premierleague/badges/t39.svg"
+    }
+  ]
